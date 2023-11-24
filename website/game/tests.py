6f9a6e7c6e62d8a0490bfbc21fcc331dac100e9d
KyleWAJohnston <kylew.johnston@gmail.com>
2023-11-24 12:51:16 -0800

    Finished test cases.

--- website/game/tests.py
+++ website/game/tests.py
@@ -29,6 +29,7 @@ class BoardTestsCase(TestCase):
 
         # Move player 2 out of the way.
         player2.col = 0
+        player2.row = 0
         player2.save()
 
         # Move the player to the bottom.
@@ -49,11 +50,12 @@ class BoardTestsCase(TestCase):
 
         # Move player 2 out of the way.
         player2.col = 0
+        player2.row = 9
         player2.save()
 
         # Move the player to the bottom.
-        player2.col = 9
-        player1.row = 9
+        player1.col = 9
+        player1.row = 0
         player1.save()
 
         # After player is at the top, move the player up again. Position should not change.
@@ -68,6 +70,7 @@ class BoardTestsCase(TestCase):
         player2 = Player.objects.get(tag=2)
 
         # Move player 2 out of the way.
+        player2.col = 9
         player2.row = 0
         player2.save()
 
@@ -83,22 +86,66 @@ class BoardTestsCase(TestCase):
         # Assert that the player is at, not beyond, the left border.
         self.assertEqual(player1.col, 0)
 
-    def test_left_border(self):
+    def test_right_border(self):
         player1 = Player.objects.get(tag=1)
         player2 = Player.objects.get(tag=2)
 
         # Move player 2 out of the way.
+        player2.col = 0
         player2.row = 0
         player2.save()
 
-        # Move the player to the left.
-        player2.col = 0
+        # Move the player to the right.
+        player2.col = 9
         player1.row = 9
         player1.save()
 
-        # After player is at the right, move the player left again. Position should not change.
+        # After player is at the left, move the player left again. Position should not change.
         self.client.post(reverse('move_player', args=(player1.tag, 'left')))
         player1.refresh_from_db()
 
         # Assert that the player is at, not beyond, the left border.
         self.assertEqual(player1.row, 0)
+
+    def test_treasure_pickup(self):
+        player1 = Player.objects.get(tag=1)
+        player2 = Player.objects.get(tag=2)
+
+        # Position the players.
+        player2.col = 9
+        player2.row = 9
+        player2.save()
+        player1.col = 0
+        player1.row = 0
+        player1.save()
+
+        # Set a treasure bellow player 1.
+        tile0x1 = Board.objects.get(col=0, row=1)
+        tile0x1.value = 3
+        tile0x1.save()
+
+        # Move player 1 into the treasure tile.
+        self.client.post(reverse('move_player', args=(player1.tag, 'down')))
+        player1.refresh_from_db()
+
+        # Assert that player 1 has a score of 3.
+        self.assertEqual(player1.score, 3)
+
+    def test_player_collision(self):
+        player1 = Player.objects.get(tag=1)
+        player2 = Player.objects.get(tag=2)
+
+        # Position the players next to each-other.
+        player2.col = 0
+        player2.row = 0
+        player2.save()
+        player1.col = 0
+        player1.row = 1
+        player1.save()
+
+        # Attempt to move player 1 into player 2.
+        self.client.post(reverse('move_player', args=(player1.tag, "up")))
+        player1.refresh_from_db()
+
+        # Assert that player 1 was not able to move and is in the original position.
+        self.assertEqual(player1.row, 1)
